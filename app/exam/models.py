da3e2cc2ac569444e28943101f38a34240f2f3d7
Ravish <ravishkumarkhg6@gmail.com>
2021-04-02 08:42:33 +0000

    exam model created

--- app/exam/models.py
+++ app/exam/models.py
@@ -6,11 +6,49 @@ from django.urls import reverse
 from django.shortcuts import render, redirect
 import datetime
 from django.utils import timezone
-from django.utils.text import slugify
+from question.models import Question
+
+
+# Section to sort questions based on particular section
+# i.e mcq, integer, database etc to create paper.
+# Various section will contribute to be a question set, and rand field will help to position section.
+
+class QuestionSection(models.Model):
+
+    # Will introduce, how this section behaves, as well as marking schemes or any type of general instructions.
+    section_instructions = body = RichTextUploadingField(blank=True, null=True)
+
+    # Will help to priotise questions set in question paper
+    rank = models.IntegerField(default=6, blank=True, null=True)
+
+    # particular type of questions added
+    question = models.ManyToManyField(Question)
+
+
 
 class QuestionSet(models.Model): 
-    pass
+    title = models.CharField(max_length=100)
+    note = models.CharField(max_length=100)
+    created_date = models.DateTimeField(
+        blank=True, null=True, auto_now_add=True)
+
+    # Each question set consists of various sections
+
+    question_section = models.ManyToManyField(QuestionSection)    
+     
+
+
 
 
 class Exam(models.Model): 
-    pass
+    created_date = models.DateTimeField(
+        blank=True, null=True, auto_now_add=True)
+    title = models.CharField(max_length=100) 
+    instructions = RichTextUploadingField(blank=True, null=True) 
+
+    # Exam can have multiple question set, first set is default
+    question_set = models.ManyToManyField(QuestionSet) 
+    note = models.CharField(max_length=100)
+    scheduled = date = models.DateTimeField(blank=True, null=True)
+
+
